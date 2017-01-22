75c1270beddab75d2d941102f22fa97b694a3ea4
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-01-22 23:12:41 +0100

    start changing API

--- src/index.js
+++ src/index.js
@@ -1,174 +1,244 @@
+import path from 'path'
+import isRegex from 'is-regex'
 import chalk from 'chalk'
 import chokidar from 'chokidar'
 import debounce from 'debounce'
+import clone from 'clone'
 import minimatch from 'minimatch'
 
 import depGraph from './lib/dep-graph'
-import isModifiedDir from './lib/is-modified-dir'
+import isInDir from './lib/is-in-dir'
 import log from './lib/log'
 
 const defaults = {
-  debouce: 100,
+  debounce: 100,
 }
 let modifiedFiles = {}
 let modifiedDirs = []
+let removedFiles = {}
+let removedDirs = []
 let forceGlobs = []
+let isWatching = false
 let isReady = false
 
-const metalsmithIncremental = (plugin, baseDir, depCheck) => (files, metalsmith, done) => {
-  // only enable incremental builds after first build
-  if (!isReady) {
-    if (plugin.length === 3) {
-      plugin(files, metalsmith, done)
-    } else {
-      try {
-        plugin(files, metalsmith)
-        done()
-      } catch (err) {
-        done(err)
-      }
+const metalsmithIncremental = (options) => {
+  const { plugin } = options
+  let cached
+
+  switch (plugin) {
+    case 'cache':
+      return cache
+    case 'watch':
+      return watch
+
+    default:
+      return filter
+  }
+
+  function filter(files, metalsmith, done) {
+    if (!isReady) {
+      done()
+      return
     }
 
-    return
-  }
+    const filesPaths = Object.keys(files)
+    const { baseDir, depResolver } = options
 
-  let paths = Object.keys(files)
+    // first add forced globs
+    if (forceGlobs.length) {
+      forceGlobs.forEach((glob) => {
+        filesPaths.filter(minimatch.filter(glob))
+          .forEach((filePath) => {
+            modifiedFiles[filePath] = true
 
-  // first add forced globs
-  if (forceGlobs.length) {
-    forceGlobs.forEach((glob) => {
-      paths.filter(minimatch.filter(glob))
-        .forEach((path) => {
-          modifiedFiles[path] = true
-
-          log(`${chalk.yellow(path)} force update`)
-        })
-    })
-  }
-
-  // second check dependencies
-  depGraph(files, modifiedFiles, modifiedDirs, metalsmith, baseDir, depCheck)
-
-  // filter non-modified files
-  for (let i = 0, l = paths.length; i < l; i++) {
-    const path = paths[i]
-
-    if (modifiedFiles[path] || isModifiedDir(path, modifiedDirs)) continue
-
-    // eslint-disable-next-line no-param-reassign
-    delete files[path]
-  }
-
-  // execute plugin
-  if (plugin.length === 3) {
-    plugin(files, metalsmith, (err) => {
-      cleanup(err)
-    })
-  } else {
-    try {
-      plugin(files, metalsmith)
-      cleanup()
-    } catch (err) {
-      cleanup(err)
-    }
-  }
-
-  // cleaup
-  function cleanup(err) {
-    // if files got renamed -> add them to modified list
-    paths = Object.keys(files)
-
-    for (let i = 0, l = paths.length; i < l; i++) {
-      const path = paths[i]
-
-      if (!modifiedFiles[path]) {
-        modifiedFiles[path] = true
-      }
-    }
-
-    done(err)
-  }
-}
-
-metalsmithIncremental.watch = (metalsmith, options) => {
-  // eslint-disable-next-line no-param-reassign
-  options = {
-    ...defaults,
-    ...options,
-  }
-
-  if (typeof options.path === 'string') {
-    // eslint-disable-next-line no-param-reassign
-    options.path = {
-      [options.path]: options.path,
-    }
-  }
-
-  if (typeof options.dounce !== 'number') {
-    // eslint-disable-next-line no-param-reassign
-    options.dounce = defaults.debouce
-  }
-
-  const source = metalsmith.source()
-  const watcher = chokidar.watch(source, {
-    ignoreInitial: true,
-    cwd: source,
-  })
-  const debouncedBuild = debounce(triggerBuild, options.debounce)
-
-  process.on('SIGTERM', stopWatching)
-  process.on('SIGINT', stopWatching)
-  process.on('SIGQUIT', stopWatching)
-
-  watcher.on('add', modifiedFile)
-    .on('change', modifiedFile)
-    .on('unlink', modifiedFile)
-    .on('addDir', modifiedDir)
-    .on('unlinkDir', modifiedDir)
-    .on('ready', () => {
-      isReady = true
-    })
-
-  function triggerBuild() {
-    log('start')
-
-    if (options.paths) {
-      const globs = Object.keys(options.paths)
-      const modifiedFilesList = Object.keys(modifiedFiles)
-
-      globs.forEach((glob) => {
-        if (minimatch.match(modifiedFilesList, glob)) {
-          forceGlobs.push(options.paths[glob])
-        }
+            log(`${chalk.yellow(filePath)} force update`)
+          })
       })
     }
 
-    metalsmith.build((err) => {
-      if (err) throw err
+    // second check dependencies
+    depGraph(files, modifiedFiles, modifiedDirs, metalsmith, baseDir, depResolver)
 
-      modifiedFiles = {}
-      modifiedDirs = []
-      forceGlobs = []
+    // filter non-modified files
+    for (let i = 0, l = filesPaths.length; i < l; i++) {
+      const filePath = filesPaths[i]
 
-      log('done')
+      if (modifiedFiles[filePath] || isInDir(filePath, modifiedDirs)) continue
+
+      // eslint-disable-next-line no-param-reassign
+      delete files[filePath]
+    }
+  }
+
+  function cache(files, metalsmith, done) {
+    setImmediate(done)
+
+    const clonedFiles = clone(files)
+
+    if (cached) {
+      const { rename } = options
+      const renameIsFunc = typeof rename === 'function'
+      const renameIsRegex = !renameIsFunc && typeof rename === 'object' && rename.from && rename.to
+      const validRename = renameIsFunc || renameIsRegex
+
+      // delete removed Files
+      const removedFilesKeys = Object.keys(removedFiles)
+
+      for (let i = 0, l = removedFilesKeys.length; i < l; i++) {
+        let removedFileKey = removedFilesKeys[i]
+        let found = !!cached[removedFileKey]
+
+        // if file not found -> may it's renamed
+        if (!found && validRename) {
+          if (renameIsFunc) {
+            const extname = path.extname(removedFileKey)
+            const renamed = rename({
+              dirname: path.dirname(removedFileKey),
+              basename: path.basename(removedFileKey, extname),
+              ext: extname,
+            })
+
+            removedFileKey = path.join(renamed.dirname, `${renamed.basename}${renamed.ext}`)
+          } else if (renameIsRegex) {
+            removedFileKey = removedFileKey.replace(rename.from, rename.to)
+          }
+
+          found = !!cached[removedFileKey]
+        }
+
+        // remove found file
+        if (found) {
+          delete cached[removedFileKey]
+          delete removedFiles[removedFileKey]
+        }
+      }
+
+      // delete removed directories
+      const cachedKeys = Object.keys(cached)
+
+      for (let i = 0, l = cachedKeys.length; i < l; i++) {
+        const cachedKey = cachedKeys[i]
+
+        if (isInDir(cachedKey, removedDirs)) {
+          delete cached[cachedKey]
+        }
+      }
+
+      // restore cache
+      const filesToRestore = clone(cached)
+      const filesToRestoreKeys = Object.keys(filesToRestore)
+
+      for (let i = 0, l = filesToRestoreKeys.length; i < l; i++) {
+        const cachedKey = filesToRestoreKeys[i]
+
+        // eslint-disable-next-line no-param-reassign
+        files[cachedKey] = filesToRestore[cachedKey]
+      }
+    }
+
+    cached = {
+      ...cached,
+      ...clonedFiles,
+    }
+  }
+
+  function watch(files, metalsmith, done) {
+    if (isWatching) return
+    isWatching = true
+
+    setImmediate(done)
+
+    // eslint-disable-next-line no-param-reassign
+    options = {
+      ...defaults,
+      ...options,
+    }
+
+    if (typeof options.path === 'string') {
+      // eslint-disable-next-line no-param-reassign
+      options.path = {
+        [options.path]: options.path,
+      }
+    }
+
+    if (typeof options.debounce !== 'number') {
+      // eslint-disable-next-line no-param-reassign
+      options.debounce = defaults.debounce
+    }
+
+    const source = metalsmith.source()
+    const watcher = chokidar.watch(source, {
+      ignoreInitial: true,
+      cwd: source,
     })
-  }
+    const debouncedBuild = debounce(triggerBuild, options.debounce)
 
-  function modifiedFile(path) {
-    modifiedFiles[path] = true
-    log(`${chalk.yellow(path)} changed`)
-    debouncedBuild()
-  }
+    process.on('SIGTERM', stopWatching)
+    process.on('SIGINT', stopWatching)
+    process.on('SIGQUIT', stopWatching)
 
-  function modifiedDir(path) {
-    modifiedDirs.push(path)
-    log(`${chalk.yellow(path)} changed`)
-    debouncedBuild()
-  }
+    watcher.on('ready', () => { isReady = true })
+      .on('all', handleAll)
 
-  function stopWatching() {
-    watcher.close()
-    process.exit(0)
+    function triggerBuild() {
+      log('start')
+
+      if (options.paths) {
+        const globs = Object.keys(options.paths)
+        const modifiedFilesList = Object.keys(modifiedFiles)
+
+        globs.forEach((glob) => {
+          if (minimatch.match(modifiedFilesList, glob)) {
+            forceGlobs.push(options.paths[glob])
+          }
+        })
+      }
+
+      metalsmith.build((err) => {
+        if (err) throw err
+
+        modifiedFiles = {}
+        modifiedDirs = []
+        removedFiles = {}
+        removedDirs = []
+        forceGlobs = []
+
+        log('done')
+      })
+    }
+
+    function handleAll(event, path) {
+      switch (event) {
+        case 'add':
+        case 'change':
+          modifiedFiles[path] = true
+          break
+
+        case 'unlink':
+          removedFiles[path] = true
+          break
+
+        case 'addDir':
+          modifiedDirs.push(path)
+          break
+
+        case 'unlinkDir':
+          removedDirs.push(path)
+          break
+
+        default:
+          return
+      }
+
+      log(`${event} ${chalk.yellow(path)}`)
+
+      debouncedBuild()
+    }
+
+    function stopWatching() {
+      watcher.close()
+      process.exit(0)
+    }
   }
 }
 
