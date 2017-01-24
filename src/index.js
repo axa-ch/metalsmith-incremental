beb74e987e861d489bb65131d44a46e3ada63298
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-01-24 20:14:29 +0100

    improved doclets

--- src/index.js
+++ src/index.js
@@ -23,39 +23,6 @@ let isReady = false
 let isRunning = false
 
 /**
- * An object which defines renaming rules.
- *
- * @typedef {Object} RenameObject
- * @property {RegExp|string} from - A pattern to match.
- * @property {string} to - A string to replace matched value.
- */
-
-/**
- * An object which define a path.
- *
- * @typedef {Object} PathObject
- * @property {string} path.basename - The name of the file without it's extension.
- * @property {string} path.dirname - The directory path of the file.
- * @property {string} path.extname - The file extension (including the dot).
- */
-
-/**
- * A callback which defines renaming rules.
- *
- * @callback RenameFunction
- * @param {PathObject} path - The current path of the file.
- *
- * @returns {PathObject} path - The new path to be used.
- */
-
-/**
- * Paths pattern map to force rebuilding unmodified files.
- *
- * @typedef {Object} PathsObject
- * @property {Glob|string} - A `glob` or `string` map specifying which other files should run through the pipeline.
- */
-
-/**
  * Returns the selected `metalsmith-incremental` subplugin.
  * Use:
  * * `filter`: to remove unmodified files from the pipeline
@@ -65,13 +32,12 @@ let isRunning = false
  * @param {Object} [options] - Plugin options hash.
  * @param {string} [options.plugin=filter] - Specify the sub plugin to use - `filter`, `cache` or `watch`.
  * @param {string} [options.baseDir] - The baseDir to which to resolve absolute paths in dependencies (`filter` only).
- * @param {RegExp|Function} [options.depResolver] - A RegExp pattern or callback to resolve dependencies (`filter` only).
- * @param {boolean} [options.deep=false] - Whether to deeply clone files or to shallow copy them (`cache` only).
+ * @param {RegExp|DependencyResolver} [options.depResolver] - A RegExp pattern or callback to resolve dependencies (`filter` only).
  * @param {RenameObject|RenameFunction} [options.rename] - A function or object defining renaming rules (`cache` only).
- * @param {Array} [options.props=['contents']] - An array of property names to sync from cached files to new file (`cache` only).
+ * @param {string|Array[string, Array[string]]} [options.props=['contents']] - An array of property names to sync from cached files to new file (`cache` only).
  * @param {PathsObject|string} [options.paths] - A glob-pattern map which forces updates of mapped files (`watch` only).
  * @param {number} [options.delay=100] - The number of milliseconds the rebuild is delayed to wait for additional changes (`watch` only).
- * @returns {Function} - Returns the specified metalsmith sub plugin - `filter`, `cache` or `watch`.
+ * @returns {filter|cache|watch} - Returns the specified metalsmith sub plugin - `filter`, `cache` or `watch`.
  */
 const metalsmithIncremental = (options) => {
   const { plugin } = options
@@ -87,6 +53,20 @@ const metalsmithIncremental = (options) => {
       return filter
   }
 
+  /**
+   * Removes unmodified files from the pipeline by resolving:
+   * * changed, added, removed files or directories
+   * * resolving glob map matches
+   * * infer dependencies
+   *
+   * **Options**
+   * * `baseDir`
+   * * `depResolver`
+   *
+   * @param {Object} files
+   * @param {MetalSmith} metalsmith
+   * @param {Function} done
+   */
   function filter(files, metalsmith, done) {
     if (!isRunning) {
       done()
@@ -125,6 +105,18 @@ const metalsmithIncremental = (options) => {
     }
   }
 
+  /**
+   * Caches all files at the specific point in the pipeline and
+   * restores unmodified files filtered previously by `filter`.
+   *
+   * **Options**
+   * * `rename`
+   * * `props`
+   *
+   * @param {Object} files
+   * @param {MetalSmith} metalsmith
+   * @param {Function} done
+   */
   function cache(files, metalsmith, done) {
     setImmediate(done)
 
@@ -244,6 +236,17 @@ const metalsmithIncremental = (options) => {
     }
   }
 
+  /**
+   * Starts watching for file system changes inside `metalsmith.source()` directory.
+   *
+   * **Options**
+   * * `paths`
+   * * `delay`
+   *
+   * @param {Object} files
+   * @param {MetalSmith} metalsmith
+   * @param {Function} done
+   */
   function watch(files, metalsmith, done) {
     if (isWatching) {
       done()
@@ -352,3 +355,46 @@ const metalsmithIncremental = (options) => {
 }
 
 export default metalsmithIncremental
+
+/**
+ * A callback which defines renaming rules.
+ *
+ * @callback DependencyResolver
+ * @param {Object} file - The currently processed file.
+ * @param {string} baseDir - The supplied `baseDir` by `options.baseDir`.
+ *
+ * @returns {Array|null} dependecies - Returns an array of dependencies (relative to `baseDir`).
+ */
+
+/**
+ * An object which defines renaming rules.
+ *
+ * @typedef {Object} RenameObject
+ * @property {RegExp|string} from - A pattern to match.
+ * @property {string} to - A string to replace matched value.
+ */
+
+/**
+ * An object which define a path.
+ *
+ * @typedef {Object} PathObject
+ * @property {string} path.basename - The name of the file without it's extension.
+ * @property {string} path.dirname - The directory path of the file.
+ * @property {string} path.extname - The file extension (including the dot).
+ */
+
+/**
+ * A callback which defines renaming rules.
+ *
+ * @callback RenameFunction
+ * @param {PathObject} path - The current path of the file.
+ *
+ * @returns {PathObject} path - The new path to be used.
+ */
+
+/**
+ * Paths pattern map to force rebuilding unmodified files.
+ *
+ * @typedef {Object} PathsObject
+ * @property {Glob|string} - A `glob` or `string` map specifying which other files should run through the pipeline.
+ */
